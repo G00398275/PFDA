a3d2c7e60f5e18ce4c71bc96e64b9f6aa20b0480
G00398275 <G00398275@gmit.ie>
2021-09-25 10:39:58 +0100

    Adding squares.py for loops revision

---
+++ squares.py
@@ -0,0 +1,20 @@
+# squares.py ; For Loops Revision
+# Author: Ross Downey
+
+'''l = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for i in l:
+    print(i, i*i)
+
+for i in l:
+    print(i)
+    print(i*i)
+    print()
+
+for i in range(10):
+    print (i, i*i)'''
+
+for i in range(20):
+    print (f"{i:2} {i*i:6}")
+
+print (list(range(20)))
